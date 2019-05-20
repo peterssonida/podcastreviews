6e071bc45f296ba7aab10d4d8d0badf38237abfd
peterssonida <41621906+peterssonida@users.noreply.github.com>
2019-05-20 13:19:22 +0200

    Add files via upload

---
+++ registerform.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Podcastforum</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="assets\js\javan.js"></script>
+    <title></title>
+  </head>
+  <body>
+    <header>
+    </header>
+
+    <div class="register">
+      <h3>Register</h3>
+      <form name="regForm" action="register.php" onsubmit="return validateRegForm()" method="POST">
+
+        <input type="text" name="uname" placeholder="Username"><br>
+
+        <input type="text" name="email" placeholder="Email"><br>
+
+        <input type="password" name="pwd" placeholder="Password"><br>
+
+        <input type="password" name="pwdRepeat" placeholder="Confirm password"><br>
+        <input type="submit" value="Register">
+      </form>
+    </div>
+  </body>
+</html>
